0373a08fe06af91204def181e8d56d72c95f9d1b
MayowaObisesan <mayowaobi74@gmail.com>
2023-09-13 17:20:52 +0100

    Update LMAO and WLMAO project files

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
+
 # Sample Hardhat Project
+
+## LMAO AND WLMAO TOKENS
+
+This project simulates ETH AND WETH implementations. There are two tokens deployed in this project.
+A LMAO Token (LMAO, LTK) and Wrapped-LMAO Token (WLMAO, WLTK)
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
---
+++ contracts/wlmao.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract WLMAO is ERC20 {
+    address immutable LMAO_ADDRESS;
+
+    constructor(address _LMAO) ERC20("WLMAOToken", "WLTK") {
+        LMAO_ADDRESS = _LMAO;
+    }
+
+    function deposit(uint _amount) external {
+        IERC20(LMAO_ADDRESS).transferFrom(msg.sender, address(this), _amount);
+        // Mint only 92% of the token.
+        uint taxedAmount = (_amount * 92) / 100;
+        _mint(msg.sender, taxedAmount);
+    }
+
+    function withdraw(uint _amount) external {
+        require(balanceOf(msg.sender) >= _amount, "Insufficient balance");
+        IERC20(LMAO_ADDRESS).transfer(msg.sender, _amount);
+        _burn(msg.sender, _amount);
+    }
+
+    fallback() external payable {}
+
+    receive() external payable {}
+}
